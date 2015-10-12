4180c774c1bc49c93a32a14500e80f4d2919d0b5
danhdatdeo94 <danhdatdeo94@gmail.com>
2015-10-12 20:52:21 +0700

    Update AssemblyInfo.cs

--- QLKhachSan_TTNhom/Properties/AssemblyInfo.cs
+++ QLKhachSan_TTNhom/Properties/AssemblyInfo.cs
@@ -23,12 +23,12 @@ using System.Runtime.InteropServices;
 [assembly: Guid("ef9c290f-95c3-467e-a15d-b3e8e5b3455a")]
 
 // Version information for an assembly consists of the following four values:
-//
+
 //      Major Version
 //      Minor Version 
 //      Build Number
 //      Revision
-//
+
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
