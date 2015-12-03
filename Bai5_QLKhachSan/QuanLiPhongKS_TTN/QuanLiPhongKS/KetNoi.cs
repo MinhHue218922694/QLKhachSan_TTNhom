8deef42433e460abec529d4a382df47bd4f0334c
yenxichha1993 <yenxichha1993@gmail.com>
2015-12-03 23:31:42 +0700

    updating... bai 5 KS

---
+++ Bai5_QLKhachSan/QuanLiPhongKS_TTN/QuanLiPhongKS/KetNoi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLPhongKS
+{
+    public class KetNoi
+    {
+        public string GetConnect()
+        {
+            return (@"Data Source=NgocHoa;Initial Catalog=QuanLiPhongKS;Integrated Security=True");
+        }
+    }
+}
