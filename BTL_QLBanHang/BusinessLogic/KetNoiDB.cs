c648e2195865c75f8219fe088b28f39172b79870
danhdatdeo94 <newfriendsfun@gmail.com>
2015-12-04 10:10:03 +0700

    Bai6_PQD

---
+++ BTL_QLBanHang/BusinessLogic/KetNoiDB.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace BusinessLogic
+{
+    public class KetNoiDB
+    {
+        SqlConnection cn = new SqlConnection();
+        static public String getconnect()
+        {
+<<<<<<< HEAD
+            return (@"Data Source=KHANHCON-PC;Initial Catalog=Bai6_BanHangSieuThi;Integrated Security=True");
+=======
+            return (@"Data Source=KHANHCON-PC;Initial Catalog=Bai6_BanHangSieuThi;Integrated Security=True");
+>>>>>>> dcb7d6d6c4ac40a052b4accf298b0a784f01f445
+        }
+    }
+}
